1595c7ce2a78e84a9b738d9d766710e437e0369b
tech-camp <tech-camp@P-029.local>
2025-08-01 20:10:01 +0900

    Revert to direct Firebase config in code

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBdTpIcSfuuFvKMzXkO1Bg-c-Saak5IphM",
+  authDomain: "attendance-app-fc8ce.firebaseapp.com",
+  projectId: "attendance-app-fc8ce",
+  storageBucket: "attendance-app-fc8ce.firebasestorage.app",
+  messagingSenderId: "320698802923",
+  appId: "1:320698802923:web:19069f4dd6afa5ed5a14ae"
 };
 
 // Firebaseを初期化
